d676171f43d85bf932e9c94b1896fcac44496db3
maidi1990 <1243001042@qq.com>
2017-12-16 19:08:26 +0800

    在db分支编写包atomisator.db完成
    - atomisator.db第一次提交。

---
+++ atomisator.db/atomisator/db/session.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from atomisator.db.mappers import Base
+metadata = Base.metadata
+
+session = None
+
+def create_session(sqluri, global_=True):
+    engine = create_engine(sqluri)
+    metadata.create_all(engine)
+    Session = sessionmaker(bind=engine, autoflush=True, autocommit=False)
+    if global_:
+        global session
+        session = Session()
+        return session
+    else:
+        return Session()
+
+def save(obj):
+    session.add(obj)
+
+def commit():
+    session.commit()
+
+def query(mapper):
+    return session.query(mapper)
+
+def execute(*args, **kw):
+    return session.execute(*args, **kw)
